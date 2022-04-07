1fa30cb46ef09bc51a5296fc51117d414c40ab5f
emileMbeke0820 <emile.essammbeke@otto.de>
2022-04-07 15:30:02 +0200

    FEATURE DONE: DDL/create/DROP/TRUNCATE

--- databases/DDL/create_Table.sql
+++ databases/DDL/create_Table.sql
@@ -1,3 +1,11 @@
+-- dev_playground.students definition
+
+-- Drop table
+
+-- DROP TABLE dev_playground.students;
+
+-- TRUNCATE TABLE dev_playground.students;
+
 CREATE TABLE dev_playground.students (
 	student_id int4 NOT NULL,
 	first_name varchar(100) NOT NULL,
