9d9ec3e60a30ff2b2c1a395293b069877ebc5074
emileMbeke0820 <emile.essammbeke@otto.de>
2022-04-07 15:17:09 +0200

    FEATURE DONE: DDL/create

---
+++ databases/DDL/create_Table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE dev_playground.students (
+	student_id int4 NOT NULL,
+	first_name varchar(100) NOT NULL,
+	last_name varchar(100) NOT NULL,
+	age int4 NOT NULL,
+	adress varchar(100) NOT NULL,
+	semester int4 NOT NULL,
+	CONSTRAINT students_pkey PRIMARY KEY (student_id)
+);
